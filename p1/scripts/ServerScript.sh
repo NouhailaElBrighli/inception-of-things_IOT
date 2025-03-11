0b16de131db881de5c47ee1678808383d4dabca7
nouhaila <nouhaila@noukhelia.myguest.virtualbox.org>
2025-03-11 21:30:24 +0000

    fix error dpkg

--- p1/scripts/ServerScript.sh
+++ p1/scripts/ServerScript.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# doesn't work it shows always warning dpkg-preconfigure: unable to re-open stdin: No such file or directory
-export DEBIAN_FRONTEND=noninteractive
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
-export PATH=$PATH:/sbin:/usr/sbin
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools
 
-sudo apt-get update -y
-
-sudo apt-get install -y net-tools
+echo 'export PATH=$PATH:/sbin:/usr/sbin'
--- p1/scripts/WorkerScript.sh
+++ p1/scripts/WorkerScript.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # doesn't work it shows always warning dpkg-preconfigure: unable to re-open stdin: No such file or directory
-export DEBIAN_FRONTEND=noninteractive
+# export DEBIAN_FRONTEND=noninteractive
 
-export PATH=$PATH:/sbin:/usr/sbin
 
-sudo apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
-sudo apt-get install -y net-tools
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools
+
+echo 'export PATH=$PATH:/sbin:/usr/sbin'
