d12dbe5abb1acfce427ce5f02ab3d5c3a1eb2ea3
nouhaila <nouhaila@noukhelia.myguest.virtualbox.org>
2025-03-12 21:44:57 +0000

    start k3 server installation

--- p1/scripts/ServerScript.sh
+++ p1/scripts/ServerScript.sh
@@ -2,6 +2,19 @@
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools
+# install net-tools (for fconfig) and curl (for download k3s)
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools curl
 
-echo 'export PATH=$PATH:/sbin:/usr/sbin'
+# add /usr/sbin to the PATH for run the command ifconfig without sudo
+echo 'export PATH=$PATH:/usr/sbin'
+
+SERVER_IP="192.168.56.110"
+
+# install k3s
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --node-ip=$SERVER_IP
+
+# get the k3s token
+K3S_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
+
+# save the k3s token in the file
+echo $K3S_TOKEN > /vagrant/k3s_token
--- p1/scripts/WorkerScript.sh
+++ p1/scripts/WorkerScript.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# doesn't work it shows always warning dpkg-preconfigure: unable to re-open stdin: No such file or directory
-# export DEBIAN_FRONTEND=noninteractive
-
-
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools curl
 
 echo 'export PATH=$PATH:/sbin:/usr/sbin'
