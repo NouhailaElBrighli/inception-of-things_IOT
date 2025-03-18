9c61d94611b1cddf9ee4a26b121f53c91fa8b49a
nouhaila <nouhaila@noukhelia.myguest.virtualbox.org>
2025-03-18 00:52:28 +0000

    p2 in progress

--- p2/scripts/ServerScript.sh
+++ p2/scripts/ServerScript.sh
@@ -2,7 +2,10 @@
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools curl
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools curl vim docker.io
+
+# authentication to solve usage limit for docker 
+echo "something" | docker login --username "nouhaila18" --password-stdin
 
 # add /usr/sbin to the PATH for run the command ifconfig without sudo
 echo 'export PATH=$PATH:/usr/sbin' >> .bashrc
@@ -13,3 +16,7 @@ SERVER_IP="192.168.56.110"
 
 # install k3s
 curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --node-ip=$SERVER_IP
+
+# whereis kubectl /usr/local/bin/kubectl
+
+kubectl apply -f /vagrant/config/app1/deployment-config.yaml
