ed17e0b711463891c111431f818577184792b237
nouhaila <nouhaila@noukhelia.myguest.virtualbox.org>
2025-03-22 03:12:20 +0000

    remove configMap and use volume hostpath

--- p2/scripts/ServerScript.sh
+++ p2/scripts/ServerScript.sh
@@ -5,7 +5,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y net-tools curl vim docker.io
 
-echo "something" | docker login --username "nouhaila18" --password-stdin
+# echo "something" | docker login --username "nouhaila18" --password-stdin
 
 # Add alias for kubectl
 echo 'alias k="kubectl"' >> .bashrc
@@ -17,14 +17,10 @@ SERVER_IP="192.168.56.110"
 # install k3s
 curl -sfL https://get.k3s.io | sh -s - server --write-kubeconfig-mode 644 --node-ip=$SERVER_IP
 
-kubectl apply -f /vagrant/config/app1/configmap.yaml
 kubectl apply -f /vagrant/config/app1/deployment-service.yaml
 
-kubectl apply -f /vagrant/config/app2/configmap.yaml
 kubectl apply -f /vagrant/config/app2/deployment-service.yaml
 
-
-kubectl apply -f /vagrant/config/app3/configmap.yaml
 kubectl apply -f /vagrant/config/app3/deployment-service.yaml
 
 kubectl apply -f /vagrant/config/ingress.yaml
